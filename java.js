b4c28cbca47712e2f6026e3587a1736bbdf455c2
Logan Hindley <117318139+logie87@users.noreply.github.com>
2024-03-29 21:27:11 -0600

    Initial Upload
    Website Initial Upload

---
+++ java.js
@@ -0,0 +1,8 @@
+// Sets side menu to 300px and uses transition from custom.css to gradually open it.
+function NavMenuOpen() {
+  document.getElementById("sidemenu").style.width = "300px";
+}
+// Sets side menu to 0px and uses transition from custom.css to gradually close it.
+function NavMenuClose() {
+  document.getElementById("sidemenu").style.width = "0px";
+}
